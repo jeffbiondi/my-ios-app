dc9bf0454ba153dca0067c313e045a5a9f07e2f4
Pierre Augeard <pierre.augeard@backmarket.com>
2023-11-07 17:12:59 +0100

    details: three view

---
+++ my-ios-app/my-ios-app/PipounSandbox.swift
@@ -0,0 +1,20 @@
+//
+//  PipounSandbox.swift
+//  my-ios-app
+//
+//  Created by test on 07/11/2023.
+//
+
+import SwiftUI
+
+struct PipounSandbox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PipounSandbox_Previews: PreviewProvider {
+    static var previews: some View {
+        PipounSandbox()
+    }
+}
--- my-ios-app/my-ios-app/ProductDetailsView.swift
+++ my-ios-app/my-ios-app/ProductDetailsView.swift
@@ -7,107 +7,54 @@
 
 import SwiftUI
 
-import SwiftUI
-
-struct CustomProgressBar: View {
-    @Binding var progress: Double
-    
-    var body: some View {
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.green]), startPoint: .leading, endPoint: .trailing)
-                .frame(height: 10)
-                .cornerRadius(5)
-            
-            HStack {
-                Image(systemName: "arrow.left.circle.fill")
-                    .foregroundColor(.white)
-                    .padding(.leading, 8)
-                
-                GeometryReader { geometry in
-                    Rectangle()
-                        .fill(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.green]), startPoint: .leading, endPoint: .trailing))
-                        .frame(width: geometry.size.width * CGFloat(progress), height: 10)
-                        .cornerRadius(5)
-                }
-                
-                Image(systemName: "arrow.right.circle.fill")
-                    .foregroundColor(.white)
-                    .padding(.trailing, 8)
-            }
-        }
-    }
-}
-
-
-struct SandboxView: View {
-    @State private var carbonMeter = 0.0
-    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
-    
-    @State private var progress: Double = 0.5
-    var body: some View {
-        ProgressView("Carbon Meter", value: carbonMeter, total: 100)
-            .onReceive(timer) { _ in
-                if carbonMeter < 100 {
-                    carbonMeter += 2
-                }
-            }
-        
-        CustomProgressBar(progress: $progress)
-            .frame(height: 40)
-            .padding(16)
-        
-        Button("Increase Progress") {
-            withAnimation {
-                progress += 0.1
-            }
-        }
-    }
-}
-
-struct ProductDetailsView: View {
+struct ProductDetailsItemView: View {
     var productCode: String
     var imageName: String
 
     var body: some View {
         VStack {
-            Text("My Product Details")
+            Text(productCode)
+                .font(.largeTitle)
             
-            VStack {
-                Image("iphone")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 300, height: 400)
-                
-                Text(productCode)
-                    .font(.headline)
-                    .padding(.top, 8)
-            }
-            .padding()
-            .background(Color.white)
-            .cornerRadius(10)
-            .shadow(radius: 5)
-            
-            
-            Text("Size: 21 x 29.7cm")
-            
-            Text("Carbon Waste avoided")
-            Text("190g")
+            Image(imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 400)
         }
         .padding()
         .background(Color.green)
     }
 }
 
-struct CustomTabView: View {
-    @State private var currentPage = 0
-    let pages: [ProductDetailsView] = [
-        ProductDetailsView(productCode: "iPhone14", imageName: "iphone"),
-        ProductDetailsView(productCode: "Salade", imageName: "iphone"),
-        ProductDetailsView(productCode: "Tomate", imageName: "iphone"),
-        ProductDetailsView(productCode: "Oignon", imageName: "iphone")
-    ]
-
+struct CarbonFootprint: View {
     var body: some View {
+        Text("90% better than new")
+            .font(.largeTitle)
+        
+    }
+}
+
+struct EWaste: View {
+    var body: some View {
+        Text("190g of eWaste gained")
+            .font(.largeTitle)
+        
+    }
+}
+
+struct ProductDetailsView: View {
+    @State private var currentPage = 0
+    var productCode: String
+    var imageName: String
+    
+    var body: some View {
+        let pages: [some View] = [
+            AnyView(ProductDetailsItemView(productCode: productCode, imageName: imageName)),
+            AnyView(CarbonFootprint()),
+            AnyView(EWaste())
+        ]
+
+
         TabView(selection: $currentPage) {
             ForEach(0..<pages.count, id: \.self) { index in
                 pages[index]
@@ -136,6 +83,6 @@ struct CustomTabView: View {
 
 struct ProductDetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        CustomTabView()
+        ProductDetailsView(productCode: "iPhone 14", imageName: "iphone")
     }
 }
