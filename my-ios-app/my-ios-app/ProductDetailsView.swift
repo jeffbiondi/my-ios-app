7b5dd3060beb11176cd470270fb191868c394040
Pierre Augeard <pierre.augeard@backmarket.com>
2023-11-07 15:47:51 +0100

    adding tab view

--- my-ios-app/my-ios-app/ProductDetailsView.swift
+++ my-ios-app/my-ios-app/ProductDetailsView.swift
@@ -39,12 +39,32 @@ struct CustomProgressBar: View {
 }
 
 
-struct ProductDetailsView: View {
+struct SandboxView: View {
     @State private var carbonMeter = 0.0
     let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
     
     @State private var progress: Double = 0.5
-    
+    var body: some View {
+        ProgressView("Carbon Meter", value: carbonMeter, total: 100)
+            .onReceive(timer) { _ in
+                if carbonMeter < 100 {
+                    carbonMeter += 2
+                }
+            }
+        
+        CustomProgressBar(progress: $progress)
+            .frame(height: 40)
+            .padding(16)
+        
+        Button("Increase Progress") {
+            withAnimation {
+                progress += 0.1
+            }
+        }
+    }
+}
+
+struct ProductDetailsView: View {
     var productCode: String
     var imageName: String
 
@@ -56,7 +76,7 @@ struct ProductDetailsView: View {
                 Image("iphone")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: 100, height: 100)
+                    .frame(width: 300, height: 400)
                 
                 Text(productCode)
                     .font(.headline)
@@ -67,37 +87,55 @@ struct ProductDetailsView: View {
             .cornerRadius(10)
             .shadow(radius: 5)
             
-           
+            
             Text("Size: 21 x 29.7cm")
-            
-            ProgressView("Carbon Meter", value: carbonMeter, total: 100)
-                .onReceive(timer) { _ in
-                    if carbonMeter < 100 {
-                        carbonMeter += 2
-                    }
-                }
-            Spacer()
-            
-            CustomProgressBar(progress: $progress)
-                .frame(height: 40)
-                .padding(16)
-            
-            Button("Increase Progress") {
-                withAnimation {
-                    progress += 0.1
-                }
-            }
             
             Text("Carbon Waste avoided")
             Text("190g")
-            
         }
         .padding()
+        .background(Color.green)
     }
 }
 
+struct CustomTabView: View {
+    @State private var currentPage = 0
+    let pages: [ProductDetailsView] = [
+        ProductDetailsView(productCode: "iPhone14", imageName: "iphone"),
+        ProductDetailsView(productCode: "Salade", imageName: "iphone"),
+        ProductDetailsView(productCode: "Tomate", imageName: "iphone"),
+        ProductDetailsView(productCode: "Oignon", imageName: "iphone")
+    ]
+
+    var body: some View {
+        TabView(selection: $currentPage) {
+            ForEach(0..<pages.count, id: \.self) { index in
+                pages[index]
+                    .tag(index)
+                    .gesture(
+                        DragGesture(minimumDistance: 10)
+                            .onEnded { value in
+                                if value.translation.width < 0 {
+                                    if currentPage < pages.count - 1 {
+                                        currentPage += 1
+                                    }
+                                } else if value.translation.width > 0 {
+                                    if currentPage > 0 {
+                                        currentPage -= 1
+                                    }
+                                }
+                            }
+                    )
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+        .background(Color.green)
+    }
+}
+
+
 struct ProductDetailsView_Previews: PreviewProvider {
     static var previews: some View {
-        ProductDetailsView(productCode: "iPhone14 Salade Tomate", imageName: "iphone")
+        CustomTabView()
     }
 }
