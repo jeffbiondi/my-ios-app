40ff05ff3cfb3fb69abbe6f65c3ea2a6087a7d9f
Jeff Biondi <jeanfrancois.biondi@gmail.com>
2023-11-07 11:26:14 +0100

    my-first-commit

---
+++ my-ios-app/my-ios-app/my_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_ios_appApp.swift
+//  my-ios-app
+//
+//  Created by Jean-Francois BIONDI on 07/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct my_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
